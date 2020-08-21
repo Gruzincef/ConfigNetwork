18f629e854a35cc27a2d12b11491962e0a204b5b
gruzincef <gruzincef@mai.ru>
2020-08-21 10:24:35 +0700

    Initial commit

---
+++ ConfigNetwork/MainForm.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace ConfigNetwork
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox AddresEthernet;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox DNS1;
+		private System.Windows.Forms.TextBox DNS2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox OldIP;
+		private System.Windows.Forms.TextBox Gateway;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.AddresEthernet = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.DNS1 = new System.Windows.Forms.TextBox();
+			this.DNS2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.OldIP = new System.Windows.Forms.TextBox();
+			this.Gateway = new System.Windows.Forms.TextBox();
+			this.NumberARM = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.NewIP = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label10 = new System.Windows.Forms.Label();
+			this.Mask = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(11, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(155, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Новый адрес сети";
+			// 
+			// AddresEthernet
+			// 
+			this.AddresEthernet.Location = new System.Drawing.Point(194, 42);
+			this.AddresEthernet.Name = "AddresEthernet";
+			this.AddresEthernet.Size = new System.Drawing.Size(204, 22);
+			this.AddresEthernet.TabIndex = 1;
+			this.AddresEthernet.Text = "192.168.0.";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(12, 167);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "DNS 1";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(12, 196);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(100, 26);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "DNS 2";
+			// 
+			// DNS1
+			// 
+			this.DNS1.Location = new System.Drawing.Point(194, 169);
+			this.DNS1.Name = "DNS1";
+			this.DNS1.Size = new System.Drawing.Size(204, 22);
+			this.DNS1.TabIndex = 6;
+			this.DNS1.Text = "8.8.8.8";
+			// 
+			// DNS2
+			// 
+			this.DNS2.Location = new System.Drawing.Point(194, 197);
+			this.DNS2.Name = "DNS2";
+			this.DNS2.Size = new System.Drawing.Size(204, 22);
+			this.DNS2.TabIndex = 7;
+			this.DNS2.Text = "8.8.0.0";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(14, 292);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 29);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Изменить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 228);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Текущий IP";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(11, 105);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 12;
+			this.label6.Text = "Шлюз";
+			// 
+			// OldIP
+			// 
+			this.OldIP.Enabled = false;
+			this.OldIP.Location = new System.Drawing.Point(194, 225);
+			this.OldIP.Name = "OldIP";
+			this.OldIP.Size = new System.Drawing.Size(204, 22);
+			this.OldIP.TabIndex = 13;
+			// 
+			// Gateway
+			// 
+			this.Gateway.Location = new System.Drawing.Point(194, 106);
+			this.Gateway.Name = "Gateway";
+			this.Gateway.Size = new System.Drawing.Size(204, 22);
+			this.Gateway.TabIndex = 14;
+			// 
+			// NumberARM
+			// 
+			this.NumberARM.Location = new System.Drawing.Point(194, 73);
+			this.NumberARM.Name = "NumberARM";
+			this.NumberARM.Size = new System.Drawing.Size(204, 22);
+			this.NumberARM.TabIndex = 17;
+			this.NumberARM.Text = "1";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(11, 76);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(174, 23);
+			this.label7.TabIndex = 18;
+			this.label7.Text = "Начальный номер АРМ";
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(14, 256);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(100, 23);
+			this.label9.TabIndex = 21;
+			this.label9.Text = "Новый IP";
+			// 
+			// NewIP
+			// 
+			this.NewIP.Location = new System.Drawing.Point(194, 253);
+			this.NewIP.Name = "NewIP";
+			this.NewIP.Size = new System.Drawing.Size(204, 22);
+			this.NewIP.TabIndex = 22;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(228, 341);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(170, 29);
+			this.button2.TabIndex = 23;
+			this.button2.Text = "Удалить настройки";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// label10
+			// 
+			this.label10.Location = new System.Drawing.Point(12, 135);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(152, 23);
+			this.label10.TabIndex = 24;
+			this.label10.Text = "Маска";
+			// 
+			// Mask
+			// 
+			this.Mask.Location = new System.Drawing.Point(194, 138);
+			this.Mask.Name = "Mask";
+			this.Mask.Size = new System.Drawing.Size(204, 22);
+			this.Mask.TabIndex = 25;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(228, 292);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(170, 29);
+			this.button3.TabIndex = 28;
+			this.button3.Text = "Сохранить настройки";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+									this.помощьToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(410, 28);
+			this.menuStrip1.TabIndex = 29;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// помощьToolStripMenuItem
+			// 
+			this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+			this.помощьToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+			this.помощьToolStripMenuItem.Text = "О программе";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(410, 384);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.Mask);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.NewIP);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.NumberARM);
+			this.Controls.Add(this.Gateway);
+			this.Controls.Add(this.OldIP);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.DNS2);
+			this.Controls.Add(this.DNS1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.AddresEthernet);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(428, 431);
+			this.MinimumSize = new System.Drawing.Size(428, 431);
+			this.Name = "MainForm";
+			this.Text = "ConfigNetwork";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox Mask;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox NewIP;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox NumberARM;
+	}
+}
---
+++ ConfigNetwork/MainForm.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Management;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices ;
+using System.Threading;
+using System.Net.NetworkInformation;
+using System.Windows;
+using  System.Diagnostics;
+
+
+namespace ConfigNetwork
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			LoadConfigARM();
+			LoadConfig();
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+
+
+void Button1Click(object sender, EventArgs e){
+	DialogResult dialogresult=MessageBox.Show("Изменить настройки сетевого интерфейса?", "Изменить настройки сетевого интерфейса?", MessageBoxButtons.YesNo);
+	if(dialogresult==DialogResult.Yes){
+		NewIp();
+		SaveConfig();
+		SetIP(NewIP.Text, Mask.Text, Gateway.Text, DNS1.Text+","+DNS2.Text);
+	}
+}
+private NtwrkIntrfc[] ListNetworkInterface(){
+	NetworkInterface[] nics=NetworkInterface.GetAllNetworkInterfaces();
+	NtwrkIntrfc[] device = new NtwrkIntrfc[nics.Length];
+	for(int x=0;x<=nics.Length-1;x++){
+		device[x]=new NtwrkIntrfc();
+		if(nics[x].GetIPProperties().UnicastAddresses.Count!=0){
+			device[x]._IP=nics[x].GetIPProperties().UnicastAddresses[0].IPv4Mask.ToString();
+		}
+		device[x]._ID=nics[x].Id;
+		device[x]._Description=nics[x].Description;
+		device[x]._ID=nics[x].Id;
+		device[x]._Name=nics[x].Name;
+		device[x]._MAC=nics[x].GetPhysicalAddress().ToString();
+		if(nics[x].GetIPProperties().DhcpServerAddresses.Count!=0)
+			for(int y=0;y<=nics[x].GetIPProperties().DhcpServerAddresses.Count-1;y++)
+				device[x]._DHCP.Add(nics[x].GetIPProperties().DhcpServerAddresses[y].ToString());
+				
+		if(nics[x].GetIPProperties().DnsAddresses.Count!=0)
+			for(int y=0;y<=nics[x].GetIPProperties().DnsAddresses.Count-1;y++)
+				device[x]._DNS.Add(nics[x].GetIPProperties().DnsAddresses[y].ToString());
+		
+		if(nics[x].GetIPProperties().GatewayAddresses.Count!=0)
+			for(int y=0;y<=nics[x].GetIPProperties().GatewayAddresses.Count-1;y++)
+				device[x]._Gateway.Add(nics[x].GetIPProperties().GatewayAddresses[y].ToString());
+	
+		}
+	return device;
+}
+
+void Button3Click(object sender, EventArgs e){
+SaveConfig();		
+NewIp();
+}
+
+private void NewIp(){
+ NewIP.Text=_NewIP=AddresEthernet.Text.Substring(0,AddresEthernet.Text.Length-1)+_IP.Substring(_IP.LastIndexOf(".")+1,_IP.Length-_IP.LastIndexOf(".")-1);
+}
+private void LoadConfigARM(){
+	IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+	NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+	if (nics == null || nics.Length < 1) return;
+    foreach (NetworkInterface adapter in nics) 
+    	if((adapter.NetworkInterfaceType==NetworkInterfaceType.Ethernet)&&(adapter.OperationalStatus== OperationalStatus.Up)){
+        IPInterfaceProperties properties = adapter.GetIPProperties();
+        
+     //   _CurrentMask=adapter.GetPhysicalAddress;
+         if(properties.DnsAddresses[0]!=null)
+        _CurrentDNS1=properties.DnsAddresses[0].ToString();
+        if(properties.DnsAddresses[1]!=null)
+        _CurrentDNS2=properties.DnsAddresses[1].ToString();
+        if(properties.GatewayAddresses.Count>0)
+       		_CurrentGateway=properties.GatewayAddresses[0].Address.ToString();
+        
+		UnicastIPAddressInformationCollection  uniCast   = properties.UnicastAddresses;
+		if (uniCast  != null){
+			_IP=uniCast[uniCast.Count-1].Address.ToString();
+			_CurrentMask=uniCast[uniCast.Count-1].IPv4Mask.ToString();
+			_CurrentNumberARM=_IP.Substring(_IP.LastIndexOf(".")+1,_IP.Length-_IP.LastIndexOf(".")-1);
+			_CurrentAddresEthernet=_IP.Substring(0,_IP.LastIndexOf("."))+".0";
+		}
+
+}
+
+}
+private string _NewIP;
+private string _IP;
+private string _CurrentNumberARM;
+private string _CurrentGateway;
+private string _CurrentDNS1;
+private string _CurrentDNS2;
+private string _CurrentAddresEthernet;
+private string _CurrentMask;
+
+private void LoadConfig(){
+		if(System.IO.File.Exists("cnfg.txt")){
+		string[] s= System.IO.File.ReadAllLines("cnfg.txt");
+		if(s.Length>0) AddresEthernet.Text=s[0];
+		if(s.Length>1) NumberARM.Text=s[1];
+		if(s.Length>2) Gateway.Text=s[2];
+		if(s.Length>3) DNS1.Text=s[3];
+		if(s.Length>4) DNS2.Text=s[4];
+		if(s.Length>5) Mask.Text=s[5];
+		}else{
+		AddresEthernet.Text=_CurrentAddresEthernet;
+		NumberARM.Text=_CurrentNumberARM;
+		Gateway.Text=_CurrentGateway;
+		Mask.Text=_CurrentMask;
+		DNS1.Text=_CurrentDNS1;
+		DNS2.Text=_CurrentDNS2;
+		Mask.Text=_CurrentMask;
+	}
+	OldIP.Text=_IP;
+	NewIp();
+}
+		
+private void SaveConfig(){
+	if(System.IO.File.Exists("cnfg.txt"))
+		System.IO.File.Delete("cnfg.txt");
+		string[] s= new String[6];
+		s[0]=AddresEthernet.Text;
+		s[1]=NumberARM.Text;
+		s[2]=Gateway.Text;
+		s[3]=DNS1.Text;
+		s[4]=DNS2.Text;
+		s[5]=Mask.Text;
+		System.IO.File.WriteAllLines("cnfg.txt",s);
+
+}
+void Button2Click(object sender, EventArgs e){
+	DialogResult dialogresult=MessageBox.Show("Удалить настройки?", "Удалить настройки?", MessageBoxButtons.YesNo);
+		if(dialogresult==DialogResult.Yes){
+		if(System.IO.File.Exists("cnfg.txt"))
+			System.IO.File.Delete("cnfg.txt");
+		LoadConfigARM();
+		LoadConfig();	
+	}
+}
+private void SetIP(string ipAddress, string subnetMask, string gateway, string DNS)  {
+			using (var networkConfigMng = new ManagementClass("Win32_NetworkAdapterConfiguration")){
+            using (var networkConfigs = networkConfigMng.GetInstances()){
+                foreach (var managementObject in networkConfigs.Cast<ManagementObject>().Where(managementObject => (bool)managementObject["IPEnabled"])){
+                    using (var newIP = managementObject.GetMethodParameters("EnableStatic")) {
+                        // Set new IP address and subnet if needed
+                        if ((!String.IsNullOrEmpty(ipAddress)) || (!String.IsNullOrEmpty(subnetMask))) {
+                            if (!String.IsNullOrEmpty(ipAddress)) {
+                                newIP["IPAddress"] = new[] { ipAddress };
+                            }
+                            if (!String.IsNullOrEmpty(subnetMask)){
+                                newIP["SubnetMask"] = new[] { subnetMask };
+                            }
+                            managementObject.InvokeMethod("EnableStatic", newIP, null);
+                        }
+                        // Set mew gateway if needed
+                        if (!String.IsNullOrEmpty(gateway)) {
+                            using (var newGateway = managementObject.GetMethodParameters("SetGateways")){
+                                newGateway["DefaultIPGateway"] = new[] { gateway };
+                                newGateway["GatewayCostMetric"] = new[] { 1 };
+                                managementObject.InvokeMethod("SetGateways", newGateway, null);
+                            }
+                        }
+                       //newIP["DNSServerSearchOrder"] = DNS.Split(',');
+            			//ManagementBaseObject setDNS = newIP.iInvokeMethod("SetDNSServerSearchOrder", newIP, null);
+   					}
+					ManagementBaseObject newDNS =managementObject.GetMethodParameters("SetDNSServerSearchOrder");
+           			newDNS["DNSServerSearchOrder"] = DNS.Split(',');
+            		ManagementBaseObject setDNS =managementObject.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+          		}
+			}
+		}
+}
+		
+}
+public class NtwrkIntrfc
+	{
+		public string _ID;
+		public string _Name;
+		public string _Description;
+		public string _IP;
+		public List<string> _Gateway=new List<string>();
+		public string  _Mask;
+		public string  _MAC;
+		public List<string> _DHCP=new List<string>();
+		public List<string> _DNS = new List<string>();
+		public Object _Obj;
+		//public List<string> 			
+	}
+	
+//*******************************************************
+public class NetworkDrive
+{
+private enum ResourceScope
+{
+RESOURCE_CONNECTED = 1,
+RESOURCE_GLOBALNET,
+RESOURCE_REMEMBERED,
+RESOURCE_RECENT,
+RESOURCE_CONTEXT
+}
+private enum ResourceType
+{
+RESOURCETYPE_ANY,
+RESOURCETYPE_DISK,
+RESOURCETYPE_PRINT,
+RESOURCETYPE_RESERVED
+}
+private enum ResourceUsage
+{
+RESOURCEUSAGE_CONNECTABLE = 0x00000001,
+RESOURCEUSAGE_CONTAINER = 0x00000002,
+RESOURCEUSAGE_NOLOCALDEVICE = 0x00000004,
+RESOURCEUSAGE_SIBLING = 0x00000008,
+RESOURCEUSAGE_ATTACHED = 0x00000010
+}
+private enum ResourceDisplayType
+{
+RESOURCEDISPLAYTYPE_GENERIC,
+RESOURCEDISPLAYTYPE_DOMAIN,
+RESOURCEDISPLAYTYPE_SERVER,
+RESOURCEDISPLAYTYPE_SHARE,
+RESOURCEDISPLAYTYPE_FILE,
+RESOURCEDISPLAYTYPE_GROUP,
+RESOURCEDISPLAYTYPE_NETWORK,
+RESOURCEDISPLAYTYPE_ROOT,
+RESOURCEDISPLAYTYPE_SHAREADMIN,
+RESOURCEDISPLAYTYPE_DIRECTORY,
+RESOURCEDISPLAYTYPE_TREE,
+RESOURCEDISPLAYTYPE_NDSCONTAINER
+}
+[StructLayout(LayoutKind.Sequential)]
+private struct NETRESOURCE
+{
+public ResourceScope oResourceScope;
+public ResourceType oResourceType;
+public ResourceDisplayType oDisplayType;
+public ResourceUsage oResourceUsage;
+public string sLocalName;
+public string sRemoteName;
+public string sComments;
+public string sProvider;
+}
+
+[DllImport("mpr.dll")]
+private static extern int WNetAddConnection2
+(ref NETRESOURCE oNetworkResource, string sPassword,
+string sUserName, int iFlags);
+
+[DllImport("mpr.dll")]
+private static extern int WNetCancelConnection2
+(string sLocalName, uint iFlags, int iForce);
+
+public static void MapNetworkDrive(string sDriveLetter, string sNetworkPath)
+{
+//Checks if the last character is \ as this causes error on mapping a drive.
+if (sNetworkPath.Substring(sNetworkPath.Length - 1, 1) == @"\")
+{
+sNetworkPath = sNetworkPath.Substring(0, sNetworkPath.Length - 1);
+}
+NETRESOURCE oNetworkResource = new NETRESOURCE()
+{
+oResourceType = ResourceType.RESOURCETYPE_DISK,
+sLocalName = sDriveLetter + ":",
+sRemoteName = sNetworkPath
+};
+
+//If Drive is already mapped disconnect the current
+//mapping before adding the new mapping
+if (IsDriveMapped(sDriveLetter))
+{
+DisconnectNetworkDrive(sDriveLetter, true);
+}
+WNetAddConnection2(ref oNetworkResource, null, null, 0);
+}
+public static int DisconnectNetworkDrive(string sDriveLetter, bool bForceDisconnect)
+{
+if (bForceDisconnect)
+{
+return WNetCancelConnection2(sDriveLetter + ":", 0, 1);
+}
+else
+{
+return WNetCancelConnection2(sDriveLetter + ":", 0, 0);
+}
+}
+public static bool IsDriveMapped(string sDriveLetter)
+{
+string[] DriveList = Environment.GetLogicalDrives();
+for (int i = 0; i < DriveList.Length; i++)
+{
+if (sDriveLetter + ":\\" == DriveList[i].ToString())
+{
+return true;
+}
+}
+return false;
+}
+}
+/*********************************************/
+	
+	
+	
+}
